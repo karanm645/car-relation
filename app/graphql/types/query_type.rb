4b5a8aed44d31cf3334810b2d72fe799221813cb
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-02-08 10:23:43 -0700

    Install graphql and updated query for vehicles

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :vehicles, String, null: false,
+      description: "returns a list of vehicles"
+    def vehicles
+      Vehicle.all
+    end
+  end
+end
